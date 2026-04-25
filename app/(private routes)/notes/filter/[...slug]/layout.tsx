f5a6b6db1582a994f3a2fd4e48a274066f48b2fe
Oksana Bochkor <oksanabockor785@gmail.com>
2026-04-25 18:33:02 +0300

    hmw

--- app/(private routes)/notes/filter/[...slug]/layout.tsx
+++ app/(private routes)/notes/filter/[...slug]/layout.tsx
@@ -2,15 +2,8 @@ import { ReactNode } from "react";
 
 export default function Layout({
   children,
-  sidebar,
 }: {
   children: ReactNode;
-  sidebar: ReactNode;
 }) {
-  return (
-    <div style={{ display: "flex", gap: "20px" }}>
-      <main>{children}</main>
-      <aside>{sidebar}</aside>
-    </div>
-  );
+  return <main>{children}</main>;
 }
