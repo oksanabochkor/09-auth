51bc6e2da84a62f0aa8b75f1956852aaa22b8612
Oksana Bochkor <oksanabockor785@gmail.com>
2026-04-25 18:10:11 +0300

    hmw

--- app/(private routes)/notes/action/create/page.tsx
+++ app/(private routes)/notes/action/create/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Create note | NoteHub",
     description: "Create a new note in NoteHub",
-    url: "https://08-zustand-mu-lime.vercel.app/notes/action/create",
+    url: "https://09-auth-one-phi.vercel.app/notes/action/create",
     images: [
       {
         url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
